5f7097f94480e9dd358edeb465ee9f36d04161a3
longvdhhe141608 <longvdhhe141608@fpt.edu.vn>
2022-10-28 11:51:53 +0700

    update addNewProject() function and create addNewAccount() function

--- src/main/java/DkDesignManagement/Service/Impl/AccountServiceImpl.java
+++ src/main/java/DkDesignManagement/Service/Impl/AccountServiceImpl.java
@@ -6,20 +6,35 @@ import DkDesignManagement.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDao accountDao;
+    private List<Account> accountList = new ArrayList<>();
+
     @Override
     public Account getAccount() {
         return null;
     }
+
     public boolean isUser(String username, String password) {
         Account account = accountDao.getAccount(username);
         if (account != null && account.getPassAcc().equals(password)) {
             return true;
         }
         return false;
+    }
+
+    public boolean isExists(String username) {
+        for (Account a : accountList) {
+            if (a.getNameAcc().equals(username) == true){
+                return false;
+            }
+        }
+        return true;
     }
 
     public AccountDao getAccountDao() {
