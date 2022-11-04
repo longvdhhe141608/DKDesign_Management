10b3881850b617d13a8c8b571fea3730f0b4cea4
longvdhhe141608 <longvdhhe141608@fpt.edu.vn>
2022-11-04 22:08:13 +0700

    fix config

---
+++ src/main/java/DkDesignManagement/Controller/UploadController.java
@@ -0,0 +1,46 @@
+package DkDesignManagement.Controller;
+
+
+import DkDesignManagement.Service.CloudinaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class UploadController {
+
+
+    @Autowired
+    CloudinaryService cloudinary;
+
+
+    @RequestMapping(value = "/upload-image", method = RequestMethod.POST)
+    public ModelAndView loadHome(HttpServletRequest request, @RequestParam("file") MultipartFile file) throws ServletException, IOException {
+
+        String url = cloudinary.uploadFile(file, "default");
+        if (ObjectUtils.isEmpty(url)) {
+            request.setAttribute("message", "upload fail ");
+
+            return new ModelAndView("upload");
+        }
+
+        request.setAttribute("message", "upload successfully : " + url);
+
+        return new ModelAndView("upload");
+    }
+
+    @RequestMapping(value = "/upload-image", method = RequestMethod.GET)
+    public ModelAndView loadHome() {
+        return new ModelAndView("upload");
+    }
+}
