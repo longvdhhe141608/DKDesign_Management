c37669aaf7843a84f6075fc3e994aa66dffb76c8
longvdhhe141608 <longvdhhe141608@fpt.edu.vn>
2022-11-15 13:54:25 +0700

    designer flows

---
+++ src/main/java/DkDesignManagement/Entity/DTO/TaskDto.java
@@ -0,0 +1,19 @@
+package DkDesignManagement.Entity.DTO;
+
+import DkDesignManagement.Entity.Tasks;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskDto {
+
+    private int sectionID;
+    private String sectionName;
+    private List<Tasks> tasksList;
+    private List<Tasks> subTasksList;
+}
---
+++ src/main/java/DkDesignManagement/Mapper/MapperTasks.java
@@ -0,0 +1,33 @@
+package DkDesignManagement.Mapper;
+
+import DkDesignManagement.Entity.Tasks;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MapperTasks implements RowMapper<Tasks> {
+
+    @Override
+    public Tasks mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Tasks tasks = Tasks.builder()
+                .id(rs.getInt("id"))
+                .projectID(rs.getInt("project_id"))
+                .sectionID(rs.getInt("section_id"))
+                .taskID(rs.getInt("task_id"))
+                .creator(rs.getInt("creator"))
+                .assignedTo(rs.getInt("section_id"))
+                .requirementID(rs.getInt("requirement_id"))
+                .taskName(rs.getString("task_name"))
+                .startingDate(rs.getDate("starting_date"))
+                .deadline(rs.getDate("deadline"))
+                .endedDate(rs.getDate("ended_date"))
+                .numberOfFile(rs.getInt("number_of_file"))
+                .description(rs.getString("description"))
+                .status(rs.getInt("status"))
+                .nameEmployee(rs.getString("name"))
+                .nameRequirement(rs.getString("requirement_name"))
+                .build();
+        return tasks;
+    }
+}
