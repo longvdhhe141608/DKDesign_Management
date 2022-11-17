73bdcfbaaa63c154066a96744a5fb47e73272a10
longvdhhe141608 <longvdhhe141608@fpt.edu.vn>
2022-11-18 01:28:13 +0700

    fix confict

--- target/DkDesginManagement-1.0/resources/assets/js/summary.js
+++
@@ -1,7 +0,0 @@
-// Update image onto the screen
-function update_image() {
-    var img = document.querySelector('img'); // Returns the first img element
-    var file = document.querySelector('input[type=file]').files[0]; // Returns the first file element found
-    img.src = window.URL.createObjectURL(file);
-
-}
