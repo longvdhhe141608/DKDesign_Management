d14ccd2f67c296f8cad95730471063f8f11715aa
NamBi2511 <96347238+NamBi2511@users.noreply.github.com>
2022-11-25 00:09:25 +0700

    fix create account

--- src/main/java/DkDesignManagement/Service/EmployeeService.java
+++ src/main/java/DkDesignManagement/Service/EmployeeService.java
@@ -1,9 +1,16 @@
 package DkDesignManagement.Service;
 
 import DkDesignManagement.Entity.Employee;
+import DkDesignManagement.Repository.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-    public Employee getEmployee();
+    @Autowired
+    EmployeeDao employeeDao = new EmployeeDao();
+    public Employee getEmployeeByEmpId(int id);
+    public Employee getEmployeeByAccId(int id);
+    public boolean emailIsExisted(String email);
+
 }
--- src/main/java/DkDesignManagement/Service/Impl/EmployeeServiceImpl.java
+++ src/main/java/DkDesignManagement/Service/Impl/EmployeeServiceImpl.java
@@ -1,11 +1,49 @@
 package DkDesignManagement.Service.Impl;
 
 import DkDesignManagement.Entity.Employee;
+import DkDesignManagement.Repository.EmployeeDao;
 import DkDesignManagement.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    /**
+     * @param id
+     * @return
+     */
     @Override
-    public Employee getEmployee() {
-        return null;
+    public Employee getEmployeeByEmpId(int id) {
+        return employeeDao.getEmployeeByEmpId(id);
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee getEmployeeByAccId(int id) {
+        return employeeDao.getInformation(id);
+    }
+
+    /**
+     * @param email
+     * @return
+     */
+    @Override
+    public boolean
+    emailIsExisted(String email) {
+        List<String> emailList = employeeDao.getEmailList();
+        for (String mail : emailList) {
+            if (mail.equals(email)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
