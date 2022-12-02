e1cb4c604067307cee47f5115528126133f73918
Nguyễn Như Huấn <115152274+huannnhe141550@users.noreply.github.com>
2022-12-02 12:03:21 +0700

    update validate

--- src/main/webapp/resources/assets/js/task-details.js
+++ src/main/webapp/resources/assets/js/task-details.js
@@ -79,16 +79,18 @@ function checkvalidate(id) {
     // console.log(today.getFullYear());
     function checkdate(today, startdate, enddate) {
         let b = 0;
-        if (today.getDate() > enddate.getDate() && today.getMonth() + 1 >= enddate.getMonth() + 1 && today.getFullYear() >= enddate.getFullYear()) {
+        if (startdate.getDate() < today.getDate() && startdate.getMonth() + 1 <= today.getMonth() + 1 && startdate.getFullYear() <= today.getFullYear()) {
             b++;
-            error[2].innerHTML = "nhập phải lớn hơn ngày hiện tại";
+            error[1].innerHTML = "nhập phải lớn hơn ngày hiện tại";
 
         }
-        else if (startdate.getDate() > enddate.getDate() && startdate.getMonth() + 1 >= enddate.getMonth() + 1 && startdate.getFullYear() >= enddate.getFullYear()) {
+        else if (startdate.getDate() >= enddate.getDate() && startdate.getMonth() + 1 >= enddate.getMonth() + 1 && startdate.getFullYear() >= enddate.getFullYear()) {
             b++;
             error[2].innerHTML = "nhập phải lớn hơn ngày bắt đầu";
+
         }
         else {
+
             return b;
         }
         return b;
