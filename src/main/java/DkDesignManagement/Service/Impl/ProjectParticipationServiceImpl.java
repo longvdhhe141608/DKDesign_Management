626f9968a374181bb9d4a8ab072f513483c5e3e8
longvdhhe141608 <longvdhhe141608@fpt.edu.vn>
2022-12-04 17:32:16 +0700

    update service

---
+++ src/main/java/DkDesignManagement/Service/Impl/ProjectParticipationServiceImpl.java
@@ -0,0 +1,29 @@
+package DkDesignManagement.Service.Impl;
+
+import DkDesignManagement.Entity.ProjectParticipation;
+import DkDesignManagement.Repository.ProjectParticipationDao;
+import DkDesignManagement.Service.ProjectParticipationService;
+import DkDesignManagement.model.MemberActiveDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ProjectParticipationServiceImpl implements ProjectParticipationService {
+    @Autowired
+    private ProjectParticipationDao projectParticipationDao;
+
+    @Override
+    public int addProjectParticipation(ProjectParticipation project) {
+        return projectParticipationDao.addProjectParticipation(project);
+    }
+
+    @Override
+    public List<MemberActiveDto> getAllMember(int projectID, int indexPage, String roleID, String textSearch) {
+        return projectParticipationDao.getAllMember(projectID, indexPage, roleID, textSearch);
+    }
+
+    @Override
+    public int totalAllMember(int projectID, String roleID, String textSearch) {
+        return projectParticipationDao.totalAllMember(projectID, roleID, textSearch);
+    }
+}
